5e42973073eca874400503fd873572e259e7ab2b
Angela Antova <antovaangela@gmail.com>
2024-05-04 19:26:55 +0200

    creating header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import logo from "../../assets/my-logo.png";
+import linkedin from "../../assets/linkedin-logo.png";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <section id="Header">
+      <div className="logo-container">
+        <img src={logo} width={150} height={150} />
+      </div>
+      <div className="navbar-container">
+        <ul>
+          <li>
+            <p>WORK</p>
+          </li>
+          <li>
+            <button>CONTACT</button>
+          </li>
+          <hr id="hr" />
+          <li>
+            <a
+              href="https://www.linkedin.com/in/angelaantova/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img id="linkedin-icon" src={linkedin} alt="linkedin-profile" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
