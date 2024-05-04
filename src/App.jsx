83aa88990583d8a92b98d6e864002c0862bb26f7
Angela Antova <antovaangela@gmail.com>
2024-05-04 20:09:43 +0200

    hero section part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { Header } from "./components/Header/Header";
+import { HeroSection } from "./components/HeroSection/HeroSection";
 
 function App() {
   return (
     <>
       <Header />
       <hr />
-
+      <HeroSection />
     </>
   );
 }
---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,27 @@
+import "./HeroSection.css";
+import hero_img from "../../assets/hero_image.jpg";
+
+export const HeroSection = () => {
+  return (
+    <section id="Hero-section">
+      <div className="hero-left">
+        <div className="hero-left-content">
+          <h1>
+            Transform your digital presence with cutting-edge technology.
+          </h1>
+          <p>
+            Hi!👋🏻 I'm a developer and marketer building fast, responsive, and
+            scalable web solutions from initial design concepts to fully functional websites.
+          </p>
+        </div>
+        <div className="hero-left-buttons">
+            <button className="contact-btn">CONTACT</button>
+            <button className="work-btn">SEE PROJECTS</button>
+        </div>
+      </div>
+      <div className="hero-right">
+        <img src={hero_img} alt="hero-img" width={500} />
+      </div>
+    </section>
+  );
+};
